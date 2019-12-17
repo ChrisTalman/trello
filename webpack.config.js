a94400185360c94dcfe18fe134218f7de4bac2ff
Chris Talman <cewe.mail@gmail.com>
2019-12-17 16:51:25 +0000

    Specified library target.

--- webpack.config.js
+++ webpack.config.js
@@ -10,37 +10,40 @@ const TYPESCRIPT_IGNORE = /(?:node_modules)$/;
 module.exports =
 {
 	mode: 'development',
-    entry:
-    {
-        index: './src/Modules/index.ts'
-    },
+	entry:
+	{
+		index: './src/Modules/index.ts'
+	},
 	target: 'node',
-    resolve:
-    {
-        extensions: [ '.js', '.ts', '.json', '.html' ],
-        alias:
-        {
-            src: __dirname + '/src',
-            common: Path.join(__dirname, '../Common')
-        }
-    },
-    output:
-    {
-        filename: '[name].js',
-        path: Path.resolve(__dirname, './')
-    },
-    watch: true,
-    module:
-    {
-        rules:
-        [
-            {
-                loader: 'ts-loader',
-                test: /\.tsx?$/,
-                exclude: TYPESCRIPT_IGNORE
-            }
-        ]
-    },
+	resolve:
+	{
+		extensions: [ '.js', '.ts', '.json' ],
+		alias:
+		{
+			root: __dirname + '/',
+			src: __dirname + '/src',
+			common: Path.join(__dirname, '../Common')
+		}
+	},
+	output:
+	{
+		filename: '[name].js',
+		path: Path.resolve(__dirname, './'),
+		libraryTarget: 'umd',
+		globalObject: 'this'
+	},
+	watch: true,
+	module:
+	{
+		rules:
+		[
+			{
+				loader: 'ts-loader',
+				test: /\.tsx?$/,
+				exclude: TYPESCRIPT_IGNORE
+			}
+		]
+	},
 	externals:
 	[
 		NodeExternals()
